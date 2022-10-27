011ade2fc55c9c122c2a7c0d62b5726a768b22f3
rafaelkoll <rafcall98@hotmail.com>
2022-10-27 16:56:52 +0100

    Beginning of yearly incidence rate

--- 720ADA.R
+++ 720ADA.R
@@ -10,7 +10,9 @@ for (p in required_packages) {
 }
 
 # Set working directory - please CHANGE ACCORDINGLY.
-setwd("/Users/rafaelkoll/Desktop/4/Masters/c/Course/ADA/Practical/Breast-cancer-incidence-analysis")
+setwd(
+  "/Users/rafaelkoll/Desktop/4/Masters/c/Course/ADA/Practical/Breast-cancer-incidence-analysis"
+)
 
 ##### POPULATION ESTIMATES - STANDARD POPULATION ####
 # Read in the population estimates files for East Anglia for the 2004-2017 and 1971-2003
@@ -28,37 +30,74 @@ pop_8017 <-
 # Load age-specific incidence rate of our population. Include everything except
 # first and last columns.
 cases_0417 <- read_excel("./Data/brca_incidence_2004_17.xlsx",
-                         skip=3,
-                         n_max=19) %>% select(everything(), -1, -last_col())
+                         skip = 3,
+                         n_max = 19) %>% select(everything(),-1,-last_col())
 
 # Add the numbers in the last row (90+ label) with the numbers in the previous to last (85-89 label)
 # since our age bands in the standard population are 1-18, with the 18th including everyone 85+.
-age_90_over <- as.numeric(as.vector(cases_0417[18,]))
-cases_0417[18,] <- cases_0417[18,] + age_90_over 
+age_90_over <- as.numeric(as.vector(cases_0417[18, ]))
+cases_0417[18, ] <- cases_0417[18, ] + age_90_over
 
 # After merging, drop the last column (over 90) -> since the data is now on the column for 85+.
-cases_0417 <- cases_0417[1:18,]
+cases_0417 <- cases_0417[1:18, ]
 
 # Convert the wide table to longer.
-cases_0417 <- cases_0417 %>% pivot_longer(cols = 1:ncol(cases_0417), names_to = "year",
-                                          values_to = "cases") %>% arrange(year)
+cases_0417 <-
+  cases_0417 %>% pivot_longer(
+    cols = 1:ncol(cases_0417),
+    names_to = "year",
+    values_to = "cases"
+  ) %>% arrange(year)
 
-# Enter age band for each year.
-cases_0417["age"] <- rep(c(1:18),times=length(unique(cases_0417$year)))
+# Enter age band for each year. Creates an array from 1 to 18,
+# repeats it n times -> n = length of unique years in the dataset.
+cases_0417["age_gp"] <-
+  rep(c(1:18), times = length(unique(cases_0417$year)))
 
+# Convert year from chr to int.
+cases_0417 <- cases_0417 %>% mutate(year = as.integer(year))
+
+# TODO: USE CROSS TABULATION TO CHECK
 
 # Read in the incidence rate for 1980 until 2003.
-cases_8003 <- read.csv("./Data/tumour.tsv", sep = '\t')
-cases_8003 <- cases_8003 %>% mutate(date_of_diagnosis = dmy(date_of_diagnosis))
+cases_8003 <- read.csv("./Data/tumour.tsv", sep = '\t') %>% mutate(date_of_diagnosis = dmy(date_of_diagnosis))
+
+# Arrange by date_of_diagnosis, patient_number.
+cases_8003 <-  cases_8003 %>% arrange(patient_number, date_of_diagnosis)
+
+remove_duplicates <- function(df) {
+  for (number in df$patient_number) {
+    print(number)
+  }
+}
 
 
-# HAVE TO PROBABLY patient_number NUMBER WITH patient.csv FILE TO GET THEIR AGE.
-# Create a new 'year' feature, keeping only the year from the date_of_diagnosis.
+  
+
+
+
+# Put the age band in the cases_8003 dataframe.
+patient <-
+  read_csv("./Data/patient.csv") %>% mutate(date_of_birth = dmy(date_of_birth))
+
+# Merge the patient and cases_8003 dataframe. Get their age at diagnosis.
+cases_8003 <- merge(patient, cases_8003, by="patient_number") %>% mutate(age_diagnosed = as.integer(interval(date_of_birth, date_of_diagnosis) / years(1)))
+
+# Get age group of patient.
+cases_8003 <- cases_8003 %>%
+  mutate(age_gp = cut(age_diagnosed, breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, Inf),
+                      labels = FALSE,
+                      right = FALSE)) 
+
+# Get the year (e.g. 1980) from the date of diagnosis.
 cases_8003 <- cases_8003 %>% mutate(year = as.numeric(format(date_of_diagnosis, '%Y')))
-# Drop date_of_diagnosis
-cases_8003 <- cases_8003 %>% select(everything(), -date_of_diagnosis)
 
-cases_8003 %>% group_by(year) %>% summarise(number=n())
+# Get cases number for each age band and each year by summarising.
+cases_8003 <- cases_8003 %>% group_by(year, age_gp) %>% summarise(cases=n())
 
-# TODO: now have all the count for year without considering age bands (basically all the age bands are in the same count).
-# Have to seperate them, probably will be able to do that when I have the patient.csv file.
+# Combine cases from 80-17.
+cases_combined <- base::rbind(cases_8003, cases_0417)
+
+# Read in european standard population
+
+
